2ee77025ef0981f480918ccf1c35aa5d23231853
sakhiimam <sakhiimam2@gmail.com>
2020-10-28 12:53:11 +0500

    incomplete

---
+++ 28oct/index.js
@@ -0,0 +1,18 @@
+
+alert(`kulsoom ansari
+ web designing`);
+ 
+var birthYear = 1998;
+alert('type of birth year is ' + typeof birthYear);
+
+var age = prompt('enter your age ');
+alert( age*365 + 'days');
+
+var num = prompt('enter ur num');
+if(num > 0) {
+    alert(`your number = ${num}
+    and it is positive`) 
+}
+else(num < 0) 
+    alert(`your number = ${num}
+    and it is negative`)
