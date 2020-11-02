4d35e01658d01e32e118f8e516e8cc44a3dade13
kulsoom ansari <kulsoomansari412@gmail.com>
2020-11-01 22:25:15 -0800

    function

--- 28oct/index.js
+++ 28oct/index.js
@@ -16,3 +16,17 @@ if(num > 0) {
 else(num < 0) 
     alert(`your number = ${num}
     and it is negative`)
+
+   function increment(argument){
+       var a = argument++;
+       return a;
+   }
+   var sum = increment(5);
+   document.write(sum);
+document.write("<br>");
+   function decrement(argument1,argument2,argument3){
+    var a = argument1-- + argument2 + --argument3;
+    return a;
+}
+var total = decrement(5,6,8);
+document.write(total);
