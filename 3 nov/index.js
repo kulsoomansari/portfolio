dad672d9c3400c9a9f0d48fe175f25bda9f5b722
sakhiimam <sakhiimam2@gmail.com>
2020-11-03 13:01:33 +0500

    js assignment

---
+++ 3 nov/index.js
@@ -0,0 +1,45 @@
+// var phrase = "the quick brown fox jumps over the lazy dog quick";
+// var replace = phrase.slice(0,17);
+// var newStr = "the white cat";
+// var mergeStr = newStr + " " + ;
+// console.log(mergeStr);
+
+// var str = prompt ("enter your sentence");
+// var newStr = str.indexOf('!');
+// if(str !== -1){
+//     console.log(`! found at address ${newStr}` )
+// }
+// else{
+//     console.log("not found")
+// }
+// console.log(newStr);
+// var str = prompt ("enter your sentence");
+// var newStr = str.indexOf(' ');
+// if(newStr !== -1){
+//     console.log("found at address" + ' '+ newStr);
+// }
+// else{
+//     console.log("not found")
+// }
+
+// var num = +prompt ('enter your number');
+// console.log(typeof num)
+// var input = prompt('enter your number');
+// var result = parseInt(input) + 5 * 6;
+// console.log(result);
+
+// var input = prompt('enter your number');
+// var result = parseFloat(input) + 5 * 6;
+// console.log(result);
+
+// var fixedDec = prompt('enter your number');
+// var formula = parseInt(fixedDec) /75*100
+// console.log(formula.toFixed(3));
+
+// var total = 3000;
+// var commision = 20;
+// var percentage = total * commision/100;
+// console.log(percentage);
+// var remaining = total- percentage;
+// console.log(remaining)
+
