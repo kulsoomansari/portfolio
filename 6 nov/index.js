004029e0008d522a64ea8fc1fd77ebdd121a5c10
unknown <kulsoomansari4122gmail.com>
2020-11-06 12:55:33 +0500

    js events

---
+++ 6 nov/index.js
@@ -0,0 +1,29 @@
+// var date = new Date();
+// function displayDate(d){
+//     console.log(date);
+// }
+// var name = "hi! kulsoom here";
+// var num = 0;
+// function myName(m){
+//     num++
+//     console.log(num + " " + name)
+// }
+// function myFunction() {
+//     document.getElementByNames("yy").innerHTML = "Hello World";
+    
+//   }
+// function myFunction(){
+//     var x = document.getElementsByName("yy");
+//     x[0].innerHTML = '1st text'
+//     x[1].innerHTML = '2nd text'
+// console.log(x.tagName);
+//   }
+
+    function myText(){
+var x = document.getElementById("para");
+if(x.innerHTML=="my dummy text"){
+    x.innerHTML = 'new text'
+}
+}
+    
+
